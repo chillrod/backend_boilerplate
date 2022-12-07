4f3ffa7a40650c07953389e411e0184ab394c0b8
chillrod <rodramone@live.com>
2022-12-06 23:14:06 -0300

    feat: boilerplate

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hi" });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running!");
+});
